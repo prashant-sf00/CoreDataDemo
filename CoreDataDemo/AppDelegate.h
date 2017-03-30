24bf387b8286a63ef763c328c32109a9513bf781
Prashant Humney <prashanthumney@Prashants-MacBook-Air.local>
2017-03-31 00:12:47 +0530

    Pushing Xcode

---
+++ CoreDataDemo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CoreDataDemo
+//
+//  Created by Prashant Humney on 14/02/17.
+//  Copyright © 2017 Prashant Humney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataDemo
+//
+//  Created by Prashant Humney on 14/02/17.
+//  Copyright © 2017 Prashant Humney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
